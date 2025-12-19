c95c8c53e2054bcded18dda9a7b1ad25199df312
Fazekas Milán <fazekas.milan1@gmail.com>
2025-12-19 13:08:47 +0100

    changed gui font, added cursorline

--- lua/options.lua
+++ lua/options.lua
@@ -3,9 +3,9 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = 'both' -- to enable cursorline!
 o.scrolloff = 15
 o.shiftwidth = 4
-o.guifont = "CaskaydiaCove Nerd Font Mono:h13"
+o.guifont = "RecMonoSmCasual Nerd Font Propo:h13"
 o.guicursor = "n-v-c-sm-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff500-blinkon500-Cursor/lCursor,i:ver25"
 o.updatetime = 0
